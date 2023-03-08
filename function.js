4d390f59126d86bb564516b5a80729d9b48e4e16
Nahuel Mussin <121046518+nadamu91@users.noreply.github.com>
2023-03-08 19:30:35 -0300

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-function abrirLinkedin() {
-    window.open("https://www.linkedin.com/feed/", "_blank");
-  }
-function abrirGithub() {
-    window.open("https://github.com/nadamu91", "_blank");
-  }
-document.getElementById("miBoton").addEventListener("click", abrirLinkedin);
-document.getElementById("miBoton2").addEventListener("click", abrirGithub);
-
